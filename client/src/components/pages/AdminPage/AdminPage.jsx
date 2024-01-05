a92810e58a4108a02b9d0f84630ba2cc00cb9b4e
Bogooo <bogdii_emanuel@yahoo.com>
2024-01-05 16:58:12 +0200

    commit ade+admin page (nu merg update si register la back)

--- client/src/components/pages/AdminPage/AdminPage.jsx
+++ client/src/components/pages/AdminPage/AdminPage.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserEdit from '../../generics/UserEdit'
 import UsersList from '../../generics/UsersList'
+
+const initialUser = {
+  id: -1,
+  name: '',
+  email: '',
+  admin: false,
+  password: ''
+}
 function AdminPage () {
+  const [user, setUser] = useState(initialUser)
+  const [refresh, setRefresh] = useState(false)
+
   return <div className="w-full flex flex-row overflow-hidden">
       <div className="w-2/3 p-10">
           <h1 className="text-blue-500 text-5xl underline">Users</h1>
-          <UsersList/>
+          <UsersList setUser={setUser} setRefresh={setRefresh} refresh={refresh}/>
       </div>
       <div className="w-1/3 p-10">
-        <UserEdit/>
+        <UserEdit user={user} setRefresh={setRefresh}/>
       </div>
   </div>
 }
